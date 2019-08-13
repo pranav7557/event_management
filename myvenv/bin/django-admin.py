c4eeb9a99d0251e7f923a3aeff6a47a76298f538
Pranav <pkatariya99@gmail.com>
2019-08-13 17:06:25 +0530

    Deploying

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pk/pasc/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- pasc/views.py
+++ pasc/views.py
@@ -10,7 +10,7 @@ import pyrebase
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("/home/pk/Downloads/pascdemo.json")
+cred = credentials.Certificate("/home/pk/pasc/pascdemo.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
